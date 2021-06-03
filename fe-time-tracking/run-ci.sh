4cd55198dc0aa3c56c7cb5b5cf4198393d4dc321
Jignesh Sheth <jigneshsheth@jigs-mac-pro.lan>
2021-06-03 09:48:39 -0500

    update fe-time-tracking v1.4

--- fe-time-tracking/run-ci.sh
+++ fe-time-tracking/run-ci.sh
@@ -6,14 +6,14 @@ export APP_IMAGE_REVISION="${2:-v1.0}"
 export APP_GIT_URL="${3:-https://github.com/jigsheth57/fe-time-tracking}"
 export APP_IMAGE_TAG="${4:-kind-registry:5000/fetimetracking}"
 
-# ytt -f build.yaml --data-value-yaml APP_NAME=$APP_NAME --data-value-yaml APP_IMAGE_REVISION=$APP_IMAGE_REVISION --data-value-yaml APP_GIT_URL=$APP_GIT_URL --data-value-yaml APP_IMAGE_TAG=$APP_IMAGE_TAG -o yaml > app-build/build.yaml
-#
-# git add .
-# git commit -m "update $APP_NAME $APP_IMAGE_REVISION"
-# git push
-
-ytt -f deploy.yaml --data-value-yaml APP_NAME=$APP_NAME --data-value-yaml APP_IMAGE_DIGEST="$(kubectl get images.kpack.io $APP_NAME -o json | jq -r '.status.latestImage')" -o yaml > app-deploy/deploy.yaml
+ytt -f build.yaml --data-value-yaml APP_NAME=$APP_NAME --data-value-yaml APP_IMAGE_REVISION=$APP_IMAGE_REVISION --data-value-yaml APP_GIT_URL=$APP_GIT_URL --data-value-yaml APP_IMAGE_TAG=$APP_IMAGE_TAG -o yaml > app-build/build.yaml
 
 git add .
 git commit -m "update $APP_NAME $APP_IMAGE_REVISION"
 git push
+
+# ytt -f deploy.yaml --data-value-yaml APP_NAME=$APP_NAME --data-value-yaml APP_IMAGE_DIGEST="$(kubectl get images.kpack.io $APP_NAME -o json | jq -r '.status.latestImage')" -o yaml > app-deploy/deploy.yaml
+#
+# git add .
+# git commit -m "update $APP_NAME $APP_IMAGE_REVISION"
+# git push
